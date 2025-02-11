cbe23609ac17429729916c70c2d3034d38af8813
Mitchel Onyebuchi <mitchelonyebuchi@gmail.com>
2025-02-10 18:23:16 -0600

    chore: Add update expense route and error handling for PUT requests

--- app/routes.py
+++ app/routes.py
@@ -61,3 +61,48 @@ def get_expenses_route():
     
     # Return the expenses data as a JSON response
     return jsonify(expenses), 200
+
+# Update Expense Route
+@main.route("/update_expense/<int:id>", methods=["PUT"])
+def update_expense_route(id):
+    data = request.get_json()
+    
+    # Error Hnadling
+    if not data:
+        return jsonify({"error": "No Data Provided"}), 400
+    
+    try:
+        # Extracting Fields
+        amount = float(data.get("amount"))
+        category = data.get("category")
+        date = data.get("date")
+        description = data.get("description")
+        payment_method = data.get("payment_method")
+        merchant = data.get("merchant")
+        recurring = int(data.get("recurring", 0))
+    
+        # Error Handling
+        if amount <= 0:
+            return jsonify({"error": "Amount must be greater than 0"}), 400
+        
+        # Validate Date Format
+            try:
+                datetime.strptime(date, "%Y-%m-%d")
+            except ValueError:
+                return jsonify({"error": "Invalid date format. Use YYYY-MM-DD"}), 400
+        
+    except ValueError:
+        return jsonify({"error": "Invalid data type for amount or recurring"}), 400
+    
+    # Basic Validation
+    if not all([amount, category, date, description, payment_method, merchant]):
+        return jsonify({"error": "Missing Required Fields"}), 400
+    
+    expense = get_expense_by_id(id)
+    
+    if not expense:
+        return jsonify({"error": "Expense Not Found"}), 404
+    
+    update_expense(id, amount, category, date, description, payment_method, merchant, recurring)
+    
+    return jsonify({"message": "Expense Updated Successfully"}), 200
